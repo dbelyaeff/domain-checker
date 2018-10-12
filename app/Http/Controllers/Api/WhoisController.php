a3c4869dfca5fbeb3afa106b32902217c601b572
Dmitriy Beliaev <admin@cogear.ru>
2018-10-13 02:06:15 +0300

    Add Punnycode support for native domain names.

--- app/Http/Controllers/Api/WhoisController.php
+++ app/Http/Controllers/Api/WhoisController.php
@@ -7,9 +7,23 @@ use Phois\Whois\Whois;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
+// use TrueBV\Punycode;
 
 class WhoisController extends Controller
 {
+    private static $Punycode;
+    private function request($domain){
+        return Cache::remember($domain, 0, function () use ($domain) {
+            $result = false;
+            // if(preg_match("/^xn/",$domain)){
+                // self::$Punycode = self::$Punycode ?? new Punycode();
+                // $domain = self::$Punycode->decode($domain);
+            // }
+            $whois = new Whois($domain);
+            $result = $whois->isAvailable();
+            return $result;
+        });
+    }
     public function get(Request $request){
         if($request->get('query')){
             $domains = preg_split("/[,\s]+/",$request->get('query'),-1,PREG_SPLIT_NO_EMPTY);
@@ -20,22 +34,16 @@ class WhoisController extends Controller
             else {
                 $root_domains = TLD::get();
             }
-            foreach($domains as $domain){
-                if(FALSE === strpos($domain,'.')){
-                    foreach($root_domains as $root_domain){
-                        $domain_with_root = join([$domain,$root_domain],'.');
-                        $result[$domain_with_root] = Cache::remember($domain_with_root,1440,function() use($domain_with_root){
-                            $whois = new Whois($domain_with_root);
-                            return $whois->isAvailable();
-                        });
+            foreach ($domains as $domain) {
+                if (false === strpos($domain, '.')) {
+                    foreach ($root_domains as $root_domain) {
+                        $domain_with_root = join([$domain,$root_domain], '.');
+                        $result[$domain_with_root] = $this->request($domain_with_root);
                     }
+                } else {
+                        $result[$domain] = $this->request($domain);
                 }
-                else {
-                    $result[$domain] = Cache::remember($domain,1440,function() use($domain){
-                        $whois = new Whois($domain);
-                        return $whois->isAvailable();
-                    });
-                }
+        
             }
             return $result;
         }
